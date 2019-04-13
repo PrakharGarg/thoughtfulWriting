42a88fbebf9861af9dd80768e1d82e2d27e5980a
Prakhar Garg <prakharg105@gmail.com>
2019-04-13 15:50:04 -0400

    add reach to input

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import './TextField.css';
 
 class TextField extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    
+        this.handleChange = this.handleChange.bind(this);
+      }
+
+      handleChange(event) {
+        this.setState({value: event.target.value});
+      }
     render() {
         return (
             <div className="TextField">
                 <div className="Input">
-                    <textarea autoFocus></textarea>
+                    <textarea autoFocus value={this.state.value} onChange={this.handleChange}></textarea>
                 </div>
             </div>  
         );
