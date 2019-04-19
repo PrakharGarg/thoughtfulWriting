3f5894d0f1b1f431733eb52a5e983becfedff5d5
Prakhar Garg <prakharg105@gmail.com>
2019-04-18 20:57:45 -0400

    added markdown and spellcheck

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './TextField.css';
+import ISimpleMDE from 'react-simplemde-v1';
+import 'simplemde/dist/simplemde.min.css';
 
 class TextField extends Component {
     constructor(props) {
@@ -13,10 +15,19 @@ class TextField extends Component {
         this.setState({value: event.target.value});
       }
     render() {
+        const option = {
+            autofocus:true,
+            toolbar: true,
+            parsingConfig: {allowAtxHeaderWithoutSpace: true},
+        };
         return (
             <div className="TextField">
                 <div className="Input">
-                    <textarea autoFocus value={this.state.value} onChange={this.handleChange}></textarea>
+                    {/* <textarea autoFocus value={this.state.value} onChange={this.handleChange}></textarea> */}
+                    <ISimpleMDE
+                        option={option}
+                        className='simplemde'
+                    />
                 </div>
             </div>  
         );
