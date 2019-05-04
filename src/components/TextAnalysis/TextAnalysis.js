9f7f55da5591e6cd109bbbd64d6d1dd9c29dd439
Prakhar Garg <prakharg105@gmail.com>
2019-05-04 15:59:26 -0400

    sentiment component

--- src/components/TextAnalysis/TextAnalysis.js
+++ src/components/TextAnalysis/TextAnalysis.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react';
-import './TextAnalysis.css';
+import React, { Component } from "react";
+import "./TextAnalysis.css";
 
 class TextAnalysis extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            sentimentScore: 0,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      sentimentScore: this.props.sentimentScore,
+      sentimentComparative: this.props.sentimentComparative
+    };
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps.sentimentScore !== this.props.sentimentScore) {
+      this.setState({ sentimentScore: this.props.sentimentScore });
     }
-    render() {
-        return (
-            <div className="TextAnalysis">
-
-            </div>  
-        );
+    if (prevProps.sentimentComparative !== this.props.sentimentComparative) {
+      this.setState({ sentimentComparative: this.props.sentimentComparative });
     }
+  }
+  render() {
+    return (
+      <div className="TextAnalysis">{this.state.sentimentComparative}</div>
+    );
+  }
 }
 
 export default TextAnalysis;
--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -1,51 +1,55 @@
-import React, { Component } from 'react';
-import './TextField.css';
-import ISimpleMDE from 'react-simplemde-v1';
-import Sentiment from 'sentiment';
-import 'simplemde/dist/simplemde.min.css';
-import TextAnalysis from '../TextAnalysis/TextAnalysis';
+import React, { Component } from "react";
+import "./TextField.css";
+import ISimpleMDE from "react-simplemde-v1";
+import Sentiment from "sentiment";
+import "simplemde/dist/simplemde.min.css";
+import TextAnalysis from "../TextAnalysis/TextAnalysis";
 
 class TextField extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-            sentimentScore: 0,
-        };
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      sentimentScore: 0,
+      sentimentComparative: 0
+    };
+  }
 
-    render() {
-        const that = this;
-        const sentiment = new Sentiment();
-        const option = {
-            autofocus:true,
-            toolbar: true,
-            parsingConfig: {allowAtxHeaderWithoutSpace: true},
-        };
-        const onEvents = {
-            change: function() {
-                that.setState({
-                    value: this.value(),
-                    sentimentScore: sentiment.analyze(this.value())
-                })
-            }
-        }
-        return (
-            <div className="TextField">
-                <div className="Input">
-                    {/* <textarea autoFocus value={this.state.value} onChange={this.handleChange}></textarea> */}
-                    <ISimpleMDE
-                        option={option}
-                        className='simplemde'
-                        onEvents={onEvents}
-                    />
-                </div>
-                <TextAnalysis
-                    sentimentScore={ this.state.sentimentScore }
-                />
-            </div>  
-        );
-    }
+  render() {
+    const that = this;
+    const sentiment = new Sentiment();
+    const option = {
+      autofocus: true,
+      toolbar: true,
+      parsingConfig: { allowAtxHeaderWithoutSpace: true }
+    };
+    const onEvents = {
+      change: function() {
+        const sentimentObj = sentiment.analyze(this.value());
+        that.setState({
+          value: this.value(),
+          sentimentScore: sentimentObj.score,
+          sentimentComparative: sentimentObj.comparative
+        });
+      }
+    };
+    return (
+      <div className="TextField">
+        <div className="Input">
+          {/* <textarea autoFocus value={this.state.value} onChange={this.handleChange}></textarea> */}
+          <ISimpleMDE
+            option={option}
+            className="simplemde"
+            onEvents={onEvents}
+          />
+        </div>
+        <TextAnalysis
+          sentimentScore={this.state.sentimentScore}
+          sentimentComparative={this.state.sentimentComparative}
+        />
+      </div>
+    );
+  }
 }
 
 export default TextField;
